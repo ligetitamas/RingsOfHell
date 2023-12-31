2d1e2fb15e27c2808a0e1eb9ca1aaa4129a20386
ligetitamas <ltommipresents@gmail.com>
2023-12-31 12:26:59 +0100

    Level structure changes

--- src/main/java/me/ltommi/ringsofhell/DungeonStructure/Level.java
+++ src/main/java/me/ltommi/ringsofhell/DungeonStructure/Level.java
@@ -6,12 +6,23 @@ import org.bukkit.configuration.ConfigurationSection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Level {
     private ArrayList<Wave> waves;
     public Level(ConfigurationSection levelSection){
         waves = new ArrayList<>();
-        for (int i=1 ; i <= levelSection.getKeys(false).size(); i++){
+
+    }
+    private void SetupWaves(ConfigurationSection levelSection){
+        int waveCount=0;
+        Set<String> keys = levelSection.getKeys(false);
+        for (String key : keys){
+            if (key.contains("wave")){
+                waveCount++;
+            }
+        }
+        for (int i=1 ; i <= waveCount; i++){
             List<Map<?, ?>> enemyList = levelSection.getMapList("wave_"+i);
             Wave newWave = new Wave();
             for (Map enemyMap : enemyList){
